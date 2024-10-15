98e8e09eb0ee3fc5da46f07ab9da3f05b5bb91c3
Edgar Novas Paniagua <edgar.novas@enti.cat>
2024-10-15 17:56:05 +0200

    Practica

---
+++ EjemploEnClase/1raPractica/Questions.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Answers.h"
+class Question {
+	std::string question;
+	Answers answers;
+	int currentIndex;
+public:
+
+	//NEW P(STD,OK,[]RESPOTES NO OK)
+
+	void AddQuestion(std::string questions,int realAnswer ,std::vector<int> falseAnswers)
+	{
+		question = questions;
+		answers.AddAnswers(realAnswer, true);
+		for (int falseAnswer : falseAnswers)
+		{
+			answers.AddAnswers(falseAnswer);
+		}
+	}
+private:
+	void AddAnswers(std::unordered_map<int,bool> map)
+	{
+		//
+		std::unordered_map<int, int> m;
+		std::vector<int> key, value;
+		for (std::unordered_map<int, int>::iterator it = m.begin(); it != m.end(); ++it) {
+			
+			answers.AddAnswers(it->first, it->second);
+			//std::cout << "Key: " << it->first << std::endl;
+			
+		}
+		//
+	}
+public:
+	void SelectQuestion(int number)
+	{
+
+	}
+
+	void ShowAnswers()
+	{
+		int contador = 1;
+		for (int answers : answers.GetAllPossibleAnswers())
+		{
+			std::cout  <<" R" << contador << " -> " << answers << std::endl;
+			contador++;
+		}
+	}
+
+};
