d567495011199e12efbc216dc33ae91154c57582
Edgar Novas <edgar.novas@enti.cat>
2024-10-29 16:49:34 +0100

    Mutex Snake

--- EjemploEnClase/EjemploEnClase/main.cpp
+++ EjemploEnClase/EjemploEnClase/main.cpp
@@ -2,8 +2,9 @@
 #include "1TutorialThreads/ThreadTutorial.h"
 #include "2InputSystem/InputSystem.h"
 #include "Utils/ConsoleControl.h"
+#include "3Nodes/Vector2.h"
 int main() {
-
+	/*
 	InputSystem* is = new InputSystem();
 
 	InputSystem::KeyBinding* kb1 = is->AddListener(K_1, []() {
@@ -32,5 +33,17 @@ int main() {
 	{
 
 	}
+	*/
+
+
+	Vector2* v = new Vector2(40, 56);
+	Vector2* v2 = new Vector2(4, 6);
+
+	//v = new Vector2(*v2);
+
+	
+	//Vector2 v3 = *v + *v2;
+
+
 
 }
